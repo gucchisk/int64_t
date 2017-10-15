2c36213c9f866d1f2cef9a31a3bbb0e94f767de2
satoshi kobayashi <gucchi@gucchis-MBP.local>
2017-10-15 20:46:16 +0900

    Fix bug of example

--- example/index.js
+++ example/index.js
@@ -2,7 +2,6 @@
 const { Int64, UInt64 } = require('../lib/int64')
 
 let i = new Int64(Buffer.from([0x80, 0x80, 0x00, 0x01, 0x01, 0x01, 0x00, 0x00]))
-let i = new Int64(0x80800001, 0x01010000)
 console.log(i.toBuffer())
 console.log(i.toString(16))
 console.log(i.shiftRight(33))
